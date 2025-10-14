7a2c7fa8db12625a64d8c3cae2d249e6d651c9d3
Matthew Eiley <matthew.eiley@mail.mcgill.ca>
2025-10-14 17:17:07 -0400

    added game simulation logic. Need to add batting simulation (markov)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+pd.set_option('display.precision', 6)
 
 def add_probabilities(df):
     df['P_WALK'] = (df['BB'] + df['HBP']) / df['PA']
@@ -6,45 +7,169 @@ def add_probabilities(df):
     df['P_2'] = df['2B'] / df['PA']
     df['P_3'] = df['3B'] / df['PA']
     df['P_HR'] = df['HR'] / df['PA']
-    df['P_OUT'] = 1 - (df['P_WALK'] + df['P_1'] + df['P_2'] + df['P_3'] + df['P_HR'])
+    df['P_SACRIFICE'] = (df['SF'] + df['SH']) / df['PA']
+    df['P_OUT'] = 1 - (df['P_WALK'] + df['P_1'] + df['P_2'] + df['P_3'] + df['P_HR'] + df['P_SACRIFICE'])
+    df['prob_total'] = df[['P_WALK', 'P_1', 'P_2', 'P_3', 'P_HR', 'P_SACRIFICE', 'P_OUT']].sum(axis=1)
 
-def get_batting_result(at_bat):
-    pass
+def simulate_at_bat(at_bat):
+    return "OUT"
 
 def simulate_game(df):
-
-    df['pos'] = 0
     runs = 0
     batter_i = -1
 
-    for j in range(9): # INNINGS
-        outs = 0
+    for _ in range(9): # INNINGS
+        outs = 0 # each inning starts with 0 outs
+        bases = 0b000 # each inning starts with empty bases
 
         while (outs < 3): # BATTERS
-            batter_i = (batter_i + 1) % len(df)
-            at_bat = df.iloc[batter_i]
-            batting_results = get_batting_result(at_bat)
-
-            outs += 1
+            batter_i = (batter_i + 1) % len(df) # cycle through batters
+            at_bat = df.iloc[batter_i] # get batter data
+            batting_results = simulate_at_bat(at_bat) # simuate at bat
 
             if batting_results == "OUT":
                 outs += 1
                 continue
 
+            if batting_results == "SACRIFICE":
+                outs += 1
+                if outs >= 3:
+                    continue
+                match bases:
+                    case 0b000:
+                        pass
+                    case 0b001:
+                        bases = 0b010
+                    case 0b010:
+                        bases = 0b100
+                    case 0b011:
+                        bases = 0b110
+                    case 0b100:
+                        bases = 0b000
+                        runs += 1
+                    case 0b101:
+                        bases = 0b010
+                        runs += 1
+                    case 0b110:
+                        bases = 0b100
+                        runs += 1
+                    case 0b111:
+                        bases = 0b110
+                        runs += 1
+                continue
+
             if batting_results == "WALK":
-                pass
+                match bases:
+                    case 0b000:
+                        bases = 0b001
+                    case 0b001:
+                        bases = 0b011
+                    case 0b010:
+                        bases = 0b011
+                    case 0b011:
+                        bases = 0b111
+                    case 0b100:
+                        bases = 0b101
+                    case 0b101:
+                        bases = 0b111
+                    case 0b110:
+                        bases = 0b111
+                    case 0b111:
+                        bases = 0b111
+                        runs += 1
+                continue
 
             if batting_results == "SINGLE":
-                df.loc[df['pos'] > 0, 'pos'] += 1
+                match bases:
+                    case 0b000:
+                        bases = 0b001
+                    case 0b001:
+                        bases = 0b011
+                    case 0b010:
+                        bases = 0b101
+                    case 0b011:
+                        bases = 0b111
+                    case 0b100:
+                        bases = 0b001
+                        runs += 1
+                    case 0b101:
+                        bases = 0b011
+                        runs += 1
+                    case 0b110:
+                        bases = 0b101
+                        runs += 1
+                    case 0b111:
+                        bases = 0b111
+                        runs += 1
+                continue
 
             if batting_results == "DOUBLE":
-                df.loc[df['pos'] > 0, 'pos'] += 2
+                match bases:
+                    case 0b000:
+                        bases = 0b010
+                    case 0b001:
+                        bases = 0b110
+                    case 0b010:
+                        bases = 0b010
+                        runs += 1
+                    case 0b011:
+                        bases = 0b110
+                        runs += 1
+                    case 0b100:
+                        bases = 0b010
+                        runs += 1
+                    case 0b101:
+                        bases = 0b110
+                        runs += 1
+                    case 0b110:
+                        bases = 0b010
+                        runs += 2
+                    case 0b111:
+                        bases = 0b110
+                        runs += 2
+                continue
 
             if batting_results == "TRIPLE":
-                df.loc[df['pos'] > 0, 'pos'] += 3
+                match bases:
+                    case 0b000:
+                        pass
+                    case 0b001:
+                        runs += 1
+                    case 0b010:
+                        runs += 1
+                    case 0b011:
+                        runs += 2
+                    case 0b100:
+                        runs +=1 
+                    case 0b101:
+                        runs += 2
+                    case 0b110:
+                        runs += 2
+                    case 0b111:
+                        runs += 3
+                bases = 0b100
+                continue
 
             if batting_results == "HOME_RUN":
-                df.loc[df['pos'] > 0, 'pos'] += 4
+                match bases:
+                    case 0b000:
+                        runs += 1
+                    case 0b001:
+                        runs += 2
+                    case 0b010:
+                        runs += 2
+                    case 0b011:
+                        runs += 3
+                    case 0b100:
+                        runs += 2
+                    case 0b101:
+                        runs += 3
+                    case 0b110:
+                        runs += 3
+                    case 0b111:
+                        runs += 4
+                bases = 0b000
+                continue
 
 def main():
     home_batting = pd.read_csv('home_batting.csv')
@@ -55,8 +180,8 @@ def main():
 
     simulate_game(home_batting)
 
-    print(home_batting)
-    print(away_batting)
+    print(home_batting.iloc[:, 10:])
+    print(away_batting.iloc[:, 10:])
 
 if __name__ == "__main__":
     main()
